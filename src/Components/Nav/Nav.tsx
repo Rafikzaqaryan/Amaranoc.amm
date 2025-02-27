6c3ee5bdff02d159fea50f5e89f2b931bba5de9b
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-27 18:21:12 +0400

    Done NavRigthPart Map Part

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,9 +1,11 @@
 import NavLeftPart from "./NavLeftPart/NavLeftPart";
+import NavRightPart from "./NavRightPart/NavRightPart";
 
 export default function Nav() {
   return (
     <div className=" pt-[20px] flex gap-5 justify-center">
       <NavLeftPart />
+      <NavRightPart />
     </div>
   );
 }
---
+++ src/Components/Nav/NavRightPart/NavRightPart.tsx
@@ -0,0 +1,9 @@
+import Map from "./Map";
+
+export default function NavRightPart() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
