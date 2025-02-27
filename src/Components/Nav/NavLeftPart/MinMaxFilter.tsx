aae390aeed7af44811e6f7aa96f0ea53dfa4689c
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-27 18:13:26 +0400

    Done NavLeftPart  MinMax part

---
+++ src/Components/Nav/NavLeftPart/MinMaxFilter.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useFilter } from "../../FilterContext";
+
+export default function MinMaxFilter() {
+  const [activeButton, setActiveButton] = useState<number | null>(null);
+  const buttonValue = ["֏", "$", "€", "₽"];
+  const { setMinPrice, setMaxPrice, minPrice, maxPrice } = useFilter();
+  const handleMinPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMinPrice(value ? parseFloat(value) : undefined);
+  };
+
+  const handleMaxPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMaxPrice(value ? parseFloat(value) : undefined);
+  };
+
+  const handleClick = (index: number) => {
+    setActiveButton(index);
+  };
+  return (
+    <div>
+      <div className="flex gap-2 pl-2 pt-[40px]">
+        <p className="text-[black]">Արժեք</p>
+        {buttonValue.map((value, index) => (
+          <button
+            key={index}
+            onClick={() => handleClick(index)}
+            className={`w-[34px] h-[34px] border flex justify-center items-center rounded-[20px] text-[18px] cursor-pointer border-[#d8d9dc] 
+          ${
+            activeButton === index
+              ? "bg-black text-white"
+              : "bg-white text-black"
+          }`}
+          >
+            {value}
+          </button>
+        ))}
+      </div>
+      <div className="flex gap-2 items-center pt-[20px]">
+        <input
+          type="number"
+          value={minPrice ?? ""}
+          placeholder="Սկսած"
+          onChange={handleMinPriceChange}
+          className="w-[120px] h-[42px] border rounded-[5px] pl-[10px] text-[14px] border-[#d8d9dc] text-[#a9a3af]"
+          min="0"
+        />
+        -
+        <input
+          type="number"
+          value={maxPrice ?? ""}
+          placeholder="Մինչև"
+          onChange={handleMaxPriceChange}
+          className="w-[120px] h-[42px] border rounded-[5px] pl-[10px] text-[14px] border-[#d8d9dc] text-[#a9a3af]"
+          min="0"
+        />
+      </div>
+    </div>
+  );
+}
