853d78c17c5f7839f185d7f6305c8c8440c1e10e
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-27 18:38:44 +0400

    Done Liked Part

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
+import Liked from "./Liked/Liked";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/liked" element={<Liked />} />
       </Routes>
     </BrowserRouter>
   );
