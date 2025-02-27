b9769ef3f1c69c34e98d02b94294d0ee2a00de5d
Rafikzaqaryan <rafikzaqaryan.ait@gmail.com>
2025-02-27 18:40:53 +0400

    Done Login part

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import Liked from "./Liked/Liked";
+import Login from "./Sign/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/liked" element={<Liked />} />
       </Routes>
     </BrowserRouter>
---
+++ src/Sign/Sign.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./Login";
+import Register from "./Register";
+import Header from "../components/header/Header";
+
+export default function Sign() {
+  return <></>;
+}
